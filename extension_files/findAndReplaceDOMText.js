4d8be3eee65499d0e8b474eb82876ace5dfcd8f8
Jennifer Hsu <jennifer.y.hsu@gmail.com>
2016-11-24 12:39:46 -0500

    matches capitalization

--- extension_files/findAndReplaceDOMText.js
+++ extension_files/findAndReplaceDOMText.js
@@ -1,13 +1,43 @@
+var DEFAULT_OLD_WORD = "girl";
+var DEFAULT_NEW_WORD = "squirrel";
+
+function matchCase(text, pattern) {
+    // uses the first letter to determine if word is capitalized; second letter to determine if all letters in word are capitalized
+    firstLetter = pattern.charCodeAt(0);
+    secondLetter = pattern.charCodeAt(1);
+
+    if (firstLetter >= 65 && firstLetter < 65 + 26) {
+      if (secondLetter >= 65 && secondLetter < 65 + 26) {
+        return text.toUpperCase();
+      }
+      return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+    }
+    return text.toLowerCase();
+}
+
 function findAndReplaceDOMText(old_word, new_word) {
   $('body :not(script)').contents().filter(function() {
     return this.nodeType === 3; // Text node
   }).each(function() {
-    this.nodeValue = this.nodeValue.replace(new RegExp(old_word, 'gi'), new_word);
+    this.nodeValue = this.nodeValue.replace(new RegExp(old_word, 'gi'), function(match) {
+        return matchCase(new_word, match);
+    });
   });
 };
 
 chrome.storage.sync.get(["old_word", "new_word"], function(items) {
-  var old_word = items.old_word || "girl";
-  var new_word = items.new_word || "squirrel";
-  $('document').delay(500).ready(findAndReplaceDOMText(old_word, new_word));
+  var old_word = items.old_word;
+  var new_word = items.new_word;
+  if (!old_word || !new_word) {
+    chrome.storage.sync.set({
+      "old_word": DEFAULT_OLD_WORD,
+      "new_word": DEFAULT_NEW_WORD,
+    }, function(){
+      old_word = DEFAULT_OLD_WORD;
+      new_word = DEFAULT_NEW_WORD;
+      $('document').delay(500).ready(findAndReplaceDOMText(old_word, new_word));
+    });
+  } else {
+    $('document').delay(500).ready(findAndReplaceDOMText(old_word, new_word));
+  }
 });
--- extension_files/options.js
+++ extension_files/options.js
@@ -1,3 +1,6 @@
+var DEFAULT_OLD_WORD = "girl";
+var DEFAULT_NEW_WORD = "squirrel";
+
 var form = document.querySelector("form");
 
 form.addEventListener("submit", function(event) {
@@ -16,11 +19,12 @@ form.addEventListener("submit", function(event) {
 chrome.storage.sync.get(["old_word", "new_word"], function(items) {
   if (!items.old_word || !items.new_word) {
     chrome.storage.sync.set({
-      "old_word": "girl",
-      "new_word": "squirrel",
-    }, function(){});
-    form.elements.old_word.value = items.old_word;
-    form.elements.new_word.value = items.new_word;
+      "old_word": DEFAULT_OLD_WORD,
+      "new_word": DEFAULT_NEW_WORD,
+    }, function(){
+      form.elements.old_word.value = DEFAULT_OLD_WORD;
+      form.elements.new_word.value = DEFAULT_NEW_WORD;
+    });
   } else {
     form.elements.old_word.value = items.old_word;
     form.elements.new_word.value = items.new_word;
