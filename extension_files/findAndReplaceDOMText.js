5105d7b580aa7db07058b88feebd4bd1ff176d80
Jennifer Hsu <jennifer.y.hsu@gmail.com>
2017-01-08 11:42:57 -0500

    fixed temporary time delay with MutationObserver; better DOM traversal

--- extension_files/findAndReplaceDOMText.js
+++ extension_files/findAndReplaceDOMText.js
@@ -12,21 +12,52 @@ function matchCase(text, pattern) {
     return text.toLowerCase();
 }
 
-function findAndReplaceDOMText(old_word, new_word) {
-  $('body :not(script) :not(iframe)').contents().filter(function() {
-    return this.nodeType === 3; // Text node
-  }).each(function() {
-    this.nodeValue = this.nodeValue.replace(new RegExp(old_word, 'gi'), function(match) {
+function handleText(node, old_word, new_word) {
+  var re = new RegExp(old_word, 'gi');
+  if (re.test(node.nodeValue)) { // prevent infinite looping
+    node.nodeValue = node.nodeValue.replace(re, function(match) {
       return matchCase(new_word, match);
     });
+  }
+}
+
+function walk(node, old_word, new_word) {
+  switch(node.nodeType) {
+    case 1: // Element
+    case 9: // Document
+    case 11: // Document fragement
+      var child = node.firstChild;
+      while (child) {
+        var next = child.nextSibling;
+        walk(child, old_word, new_word);
+        child = next;
+      }
+      break;
+    case 3: // Text node
+      handleText(node, old_word, new_word);
+      break;
+  }
+}
+
+function findAndReplaceDOMText(old_word, new_word) {
+  // watch for future changes
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      walk(mutation.target, old_word, new_word);
+    });
   });
+  observer.observe(document.body, {
+    childList: true,
+    characterData: true,
+    subtree: true
+  });
+
+  // initial pass
+  walk(document.body, old_word, new_word);
 };
 
 chrome.storage.sync.get(["old_word", "new_word"], function(items) {
   var old_word = items.old_word;
   var new_word = items.new_word;
-
-  $('document').ready(function() {
-    setTimeout(findAndReplaceDOMText.bind(null, old_word, new_word), 1000);
-  });
+  findAndReplaceDOMText(old_word, new_word);
 });
